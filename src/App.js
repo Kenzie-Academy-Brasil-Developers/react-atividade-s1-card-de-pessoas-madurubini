426192c87febd839f139675e25d8804e69234785
Madu Rubini <mariaed.rubini@gmail.com>
2021-08-31 15:29:02 -0300

    fix: correçao na visualizaçao dos cards e componente finalizado

--- src/App.js
+++ src/App.js
@@ -2,32 +2,42 @@ import "./App.css";
 import Developer from "./components/Developer/Developer";
 
 function App() {
-  let people = [
-    {
-      name: "Lucas",
-      age: 18,
-      country: "Alemanha",
-    },
-    {
-      name: "Madu",
-      age: 22,
-      country: "Brasil",
-    },
-    {
-      name: "José",
-      age: 25,
-      country: "Peru",
-    },
-  ];
+  let person1 = {
+    name: "Lucas",
+    age: 18,
+    country: "Alemanha",
+  };
 
-  let person = people.map(function (item) {
-    return [item.name, item.age, item.country];
-  });
+  let person2 = {
+    name: "Madu",
+    age: 22,
+    country: "Brasil",
+  };
+
+  let person3 = {
+    name: "José",
+    age: 25,
+    country: "Peru",
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <Developer name={person[0]} />
+        <Developer
+          name={person1.name}
+          age={person1.age}
+          country={person1.country}
+        />
+        <Developer
+          name={person2.name}
+          age={person2.age}
+          country={person2.country}
+        />
+        <Developer
+          name={person3.name}
+          age={person3.age}
+          country={person3.country}
+        />
       </header>
     </div>
   );
--- src/components/Developer/Developer.jsx
+++ src/components/Developer/Developer.jsx
@@ -1,6 +1,8 @@
+import "./style.css";
+
 function Developer({ name, age, country }) {
   return (
-    <div>
+    <div className="Card">
       <h2>Dev: {name}</h2>
       <h3>Idade: {age}</h3>
       <h3>País: {country}</h3>
